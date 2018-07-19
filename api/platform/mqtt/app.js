2801a36dea60a2eb4b158a34e821b2f7b3122891
lancelotnemoj <lancelotnemoj@outlook..com>
2018-07-19 17:25:35 +0800

    changes

--- api/platform/mqtt/app.js
+++ api/platform/mqtt/app.js
@@ -1,6 +1,5 @@
-const mosca = require('mosca');
-//构建自带服务器
-const package = require("./package.json");
+const mosca = require('mosca')
+const package = require("./package.json")
 const MqttServer = new mosca.Server({
     port: package.config.mport
 });
@@ -9,9 +8,7 @@ MqttServer.on('clientConnected', function(client) {
     //监听连接
     console.log('client connected', client.id);
 });
-/**
- * 监听MQTT主题消息
- **/
+
 MqttServer.on('published', function(packet, client) {
     //当客户端有连接发布主题消息
     console.log(`topic:${packet.topic}\tpayload:${packet.payload.toString()}`)
--- api/readme.md
+++ api/readme.md
@@ -5,3 +5,19 @@
 - platform：
     - http：方琪鸿
     - mqtt：姜佐腾
+
+#### how to use it
+mqtt
+- start server(require forever)
+```
+npm start
+```
+
+- check server
+start a pub process and sub process
+```
+npm run check
+```
+
+- change default port
+change config.port in **package.json**
