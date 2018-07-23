8fd22275114108f4546346af5e4541e4e1234740
lancelotnemoj <lancelotnemoj@outlook..com>
2018-07-23 18:03:22 +0800

    change port

--- docker/script/mqtt/mqtt.sh
+++ docker/script/mqtt/mqtt.sh
@@ -1,5 +1,8 @@
+# 构建应用
 cp ../../../api/platform/mqtt/app.js ./
 cp ../../../api/platform/mqtt/package.json ./
 cp ../../../api/platform/mqtt/package-lock.json ./
 docker build -t explatform/mqtt .
 rm -rf app.js package.json package-lock.json
+# 启动镜像
+docker run -d --name mqtt -p 8201:8080 explatform/mqtt
