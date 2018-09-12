ca667aad1bac300a4ecd9c8dd0fb3d298832d404
Zeoyloa <lancelotenmoj@outlook.com>
2018-09-12 19:21:21 +0800

    web side

---
+++ web/explatform/src/App.vue
@@ -0,0 +1,33 @@
+<template message="hello">
+  <div>
+    <md-toolbar class="md-primary">
+        <h1 class="md-title">Explatform</h1>
+    </md-toolbar>
+    
+    <md-content>
+        {{message}}
+    </md-content>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+        message:"loading..."
+      }
+    },
+    mounted(){
+      setTimeout(()=>{
+        this.message="hello"
+      },3000)
+    }
+  }
+</script>
+
+<style>
+.md-content {
+  padding: 16px;
+}
+</style>
---
+++ web/explatform/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
