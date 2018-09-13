cf7f10e967546ae040fd5c8a57b598629ff968b4
Zeoyloa <lancelotenmoj@outlook.com>
2018-09-13 16:12:50 +0800

    home page

--- web/explatform/src/main.js
+++ web/explatform/src/main.js
@@ -9,6 +9,12 @@ import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// require styles
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default global options } */ )
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
