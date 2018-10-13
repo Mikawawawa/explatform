9ec2223d456aac6542b2916aec0c8d0ba5f0f66e
shengshengqi <1296813320@qq.com>
2018-10-13 13:33:18 +0800

    service

--- web/explatform/service/request .js
+++ web/explatform/service/request .js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import axios from 'axios'
-import qs from 'qs'
 import config from "./config.json"
 let domain = config.domain
 config.post={
@@ -32,10 +31,66 @@ export default async (id,password) => {
 }
 
 export default async () => {
-    return axios [get](`${config.GET_COURSE}`, {
+    return axios [get](`${config.GET_POWER}`, {
         params: form
-      }, config.get)
-    }
+    }, config.get)
+}
 
+export default async () => {
+    return axios [get](`${config.S_GET_COURSE}`, {
+        params: form
+    }, config.get)
+}
 
-  
+export default async () => {
+    return axios [get](`${config.S_COURSE_DETAIL}`, {
+        params: form
+    }, config.get)
+}
+
+export default async (student_id,exp_id,article) => {
+    let response=await request('post',`${config.SET_REPORT}`,{
+        student_id:student_id,
+        exp_id:exp_id,
+        article:article
+    })
+    return JSON.parse(JSON.stringify(response.data))
+}
+
+export default async () => {
+    return axios [get](`${config.T_COURSE_DETAIL}`, {
+        params: form
+    }, config.get)
+}
+
+export default async (student_id,exp_id,preview,action,report) => {
+    let response=await request('post',`${config.SET_GRADE}`,{
+        student_id:student_id,
+        exp_id:exp_id,
+        preview:preview,
+        action:action,
+        report:report
+    })
+    return JSON.parse(JSON.stringify(response.data))
+}
+
+export default async () => {
+    return axios [get](`${config.GET_REPORT}`, {
+        params: form
+    }, config.get)
+}
+
+export default async () => {
+    return axios [get](`${config.T_GET_COURSE}`, {
+        params: form
+    }, config.get)
+}
+
+export default async (classroom_id,class_id, process) => {
+    let response=await request('post',`${config.SET_GRADE}`,{
+        classroom_id:classroom_id,
+        class_id:class_id,
+        process: process
+    })
+    return JSON.parse(JSON.stringify(response.data))
+}
