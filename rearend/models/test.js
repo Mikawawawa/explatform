4720e39cf41395e77a46645da528e6e094aaea75
lancelotnemoj <lancelotnemoj@outlook..com>
2018-10-14 14:19:47 +0800

    add user

--- rearend/models/test.js
+++ rearend/models/test.js
@@ -1,4 +1,10 @@
 const teacher=require("./teacher")
+const user=require("./user")
+
+/**
+ * 测试人：姜佐腾
+ * 日期：2018年10月13日
+ */
 
 // teacher.getCourse("17031803").then(data=>{
 //     console.log(data)
@@ -19,8 +25,21 @@ const teacher=require("./teacher")
 //     console.log(data)
 // })
 
-teacher.startExp(
-    '254','17040318','123'
-).then(data=>{
-    console.log(data)
-})
+// teacher.startExp(
+//     '254','17040318','123'
+// ).then(data=>{
+//     console.log(data)
+// })
+
+
+/** 
+ * 测试人：姜佐腾
+ * 日期：2018年10月14日
+*/
+// user.login('17041802',"aptx4869").then(data=>{
+//     console.log(data)
+// })
+
+// user.getPower('17041802').then(data=>{
+//     console.log(data)
+// })
--- rearend/models/user.js
+++ rearend/models/user.js
@@ -1,21 +1,46 @@
 const connection=require("./connect")
 
 /**
- * Author: 
- * Date: 
- * Time: 
+ * Author: 姜佐腾
+ * Date: 2018年10月14日
+ * Time: 13点59分
  * Description: 
  */
 exports.login=async function login(id,password) {
-    
+    let data=await connection.execute("SELECT * FROM `user` WHERE `user_id`=? and `password`=?",[id,password])
+    if(typeof(data.info)!="string"){
+        return data
+    }else{
+        data.info="LOGIN SUCCESSFUL"
+        return data
+    }
 }
 
 /**
- * Author: 
- * Date: 
+ * Author: 姜佐腾
+ * Date: 2018年10月14日
  * Time: 
- * Description: 
+ * Description: 1:学科、2:教师、3:学生
  */
 exports.getPower=async function getPower(id){
-
+    let data=await connection.execute("SELECT DISTINCT user_type FROM `user` WHERE `user_id`=?",[id])
+    if(data.status==0){
+        return data
+    }else{
+        console.log(data.info.user_type)
+        if(data.info.user_type==1){
+            data.info={
+                user_type:"学科负责人"
+            }
+        }else if(data.info.user_type==2){
+            data.info={
+                user_type:"教师"
+            }
+        }else{
+            data.info={
+                user_type:"学生"
+            }
+        }
+        return data
+    }
 }
