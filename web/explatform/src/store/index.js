ca107a46ed0aad17bf0a12bfacbf6c4a4aaa0570
shengshengqi <1296813320@qq.com>
2018-10-16 17:59:55 +0800

    store

--- web/explatform/src/store/index.js
+++ web/explatform/src/store/index.js
@@ -19,6 +19,12 @@ const store = new Vuex.Store({
       },
       setExp(state,data){
         state.Exp=JSON.parse(data)
+      },
+      getTcouse(state,data){
+        state.Tcouse=JSON.parse(data)
+      },
+      getScouse(state,data){
+        state.Tcouse=JSON.parse(data)
       }
     },
     actions:{
