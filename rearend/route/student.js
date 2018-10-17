73342264cb59a47b60d3e2d92683c8e4ae815437
Tremere12 <31583814+Tremere12@users.noreply.github.com>
2018-10-17 10:50:19 +0800

    fit bug

--- rearend/route/student.js
+++ rearend/route/student.js
@@ -3,23 +3,47 @@ const student=require("../service/StudentService")
 
 app.get("/get_course", async(req, res) => {
     let params=await student.studentGet_courseGET(req.query.student_id)
-    res.send(JSON.stringify({
-        status:params.status,
-        info:params.info
-    }))
+    if(params.status != 1){
+        res.send(JSON.stringify({
+            status: 0,
+            info: "Action error:" + params.info
+        }))
+    }                   //错误处理
+    else{    
+        res.send(JSON.stringify({
+            status:params.status,
+            info:params.info
+        }))
+    }
 })
 app.get("/get_exp", async(req, res) => {
     let params=await student.studentGet_expGET(req.query.student_id,req.query.course_id)
-    res.send(JSON.stringify({
+    if(params.status != 1){
+        res.send(JSON.stringify({
+            status: 0,
+            info: "Action error:" + params.info
+        }))
+    }                    //错误处理
+    else{
+        res.send(JSON.stringify({
         status:params.status,
         info:params.info
-    }))
+        }))
+    }
 })
 app.get("/set_report", async(req, res) => {
     let params=await student.studentSet_reportPOST(req.query.student_id,req.query.exp_id,req.query.article)
-    res.send(JSON.stringify({
-        status:params.status,
-        info:params.info
-    }))
+    if(params.status != 1){
+        res.send(JSON.stringify({
+            status: 0,
+            info: "Action error:" + params.info
+        })) 
+    }                 //错误处理
+    else{
+        res.send(JSON.stringify({
+            status:params.status,
+            info:params.info
+        }))
+    }
 })
 module.exports = app
