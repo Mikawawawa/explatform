50df6b9411af6a4f2204c274fa4469dea5817e02
shengshengqi <1296813320@qq.com>
2018-10-17 18:30:15 +0800

    text

--- web/explatform/src/view/Test.vue
+++ web/explatform/src/view/Test.vue
@@ -2,15 +2,9 @@
     <div>
         <p>{{test}}</p>
         <p>{{login}}</p>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
         <p>{{sexp}}</p>
         <p>{{texp}}</p>
         <p>{{tcourse}}</p>
->>>>>>> 19d29b0579a86b37a16392588f532cf7468eec2b
->>>>>>> 8cf7668cac1e37fcf928adc96b6ca26c1c4ec96f
         <p>{{this.$dataSource.request}}</p>
     </div>
 </template>
@@ -30,18 +24,6 @@ export default {
         this.login=await this.$dataSource.Login("17031803","aptx4869")
         this.test=await this.$dataSource.getExplist("101")
         console.log(this.test)
-<<<<<<< HEAD
-=======
-=======
-        this.test=this.$appName
-        // console.log(this.$dataSource)
-        this.login=await this.$dataSource.Login("17031803","aptx4869")
-        this.sexp=await this.$dataSource.sGetExp("17041802","101")
-        this.tcourse=await this.$dataSource.tGetCourse("17031803")
-        this.texp=await this.$dataSource.tCourseDetail("17040318")
-        // console.log(this.sexp)
->>>>>>> 19d29b0579a86b37a16392588f532cf7468eec2b
->>>>>>> 8cf7668cac1e37fcf928adc96b6ca26c1c4ec96f
     }
 }
 </script>
